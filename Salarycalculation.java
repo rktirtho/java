ba2a8d6af86c94edc597d8ac061c06b4571a91eb
Farhana <55168133+farhanarahman2299@users.noreply.github.com>
2019-11-21 22:34:44 +0600

    Add files via upload

---
+++ Salarycalculation.java
@@ -0,0 +1,15 @@
+
+package salarycalculation;
+
+public class Salarycalculation {
+
+   
+    public static void main(String[] args) {
+         int salary =50000;
+    
+    int houseRent=(salary/100)*40;
+    System.out.println((salary+houseRent)*12);
+   
+    }
+    
+}
