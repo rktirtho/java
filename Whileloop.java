83f9a1a446f01b8b9cfb5f877078b6a884e06ae4
Farhana <55168133+farhanarahman2299@users.noreply.github.com>
2019-12-05 21:10:27 +0600

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,17 @@
+
+package whileloop;
+
+
+public class Whileloop {
+
+    public static void main(String[] args) {
+      int number =1;
+		
+		while (number <= 10)  {
+			System.out.printf(" Numbers From the While Loop is: %d \n ", number);
+		}
+	}
+}
+
+    
+
